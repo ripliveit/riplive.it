606a2084fe0e15e9653ea443d02459e9e02a22e9
gabrieledarrigo <darrigo.g@gmail.com>
2014-07-10 20:59:43 +0200

    Instagram going online

--- app/scripts/controllers/photos.js
+++ app/scripts/controllers/photos.js
@@ -4,41 +4,56 @@
 angular.module('riplive')
 
 /**
- * [PhotosCtrl description]
-
+ * Load and display all riplive's photos.
+ *
  * @param {Object} $scope
  * @param {Object} socketIo
  */
-    .controller('PhotosCtrl', function PhotosCtrl($scope, socketIo) {
-        //Connect to remote websocket server.
-        var socket = socketIo.connect('http://onair.riplive.it:80');
+.controller('PhotosCtrl', function PhotosCtrl($scope, socketIo) {
+    //Connect to remote websocket server.
+    var socket = socketIo.connect('http://onair.riplive.it:80');
 
-        $scope.loading = true;
+    $scope.loading = true;
 
-        /**
-         * Draw photos grid 
-         * when the server emit the 'photos' server
-         * 
-         * @param  {Array} data
-         * @return {undefined}
-         */
-        socket.on('photos', function(data) {
-            $scope.$apply(function() {
-                $scope.loading = false;
-                $scope.photos = data;
-            });
+    /**
+     * Draw photos grid
+     * when the server emit the 'photos' server
+     *
+     * @param  {Array} data
+     * @return {undefined}
+     */
+    socket.on('photos', function(data) {
+        $scope.$apply(function() {
+            $scope.loading = false;
+            $scope.photos = data;
         });
+    });
 
-        /**
-         * Push a single photo into the
-         * photos array when the server emit the 'photo' event.
-         * 
-         * @param  {Object} data
-         * @return {undefined}
-         */
-        socket.on('photo', function(data) {
+    /**
+     * Push a single photo into the
+     * photos array when the server emit the 'photo' event.
+     * A photo can have multiple tag to which server is subscribed, so
+     * as a collateral effect, the same photo can be pushed twice.
+     * So, before pushing, the script check if a photo with the same id
+     * is already in place.
+     * 
+     * @param  {Object} data
+     * @return {undefined}
+     */
+    socket.on('photo', function(data) {
+        var found = false;
+
+        for (var i = 0, len = $scope.photos.length; i < len; i++) {
+            if ($scope.photos[i].id === data.id) {
+                found = true;
+            }
+        }
+
+        if (!found) {
             $scope.$apply(function() {
                 $scope.photos.unshift(data);
             });
-        })
+        }
     });
+});
+
