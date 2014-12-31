d86bb256411dbe5b00b84ecec15aea9bb940c567
gabrieledarrigo <darrigo.g@gmail.com>
2014-12-31 14:06:58 +0100

    Default value are the same of the development config file

--- config/default.js
+++ config/default.js
@@ -1,9 +1,6 @@
 var config = {
-    // wp_uri : 'http://riplive.local/api',
-    // admin_uri : 'http://riplive.local/wp-admin/admin-ajax.php',
-
-    wp_uri : 'http://www.static.riplive.it/api',
-    admin_uri : 'http://www.static.riplive.it/wp-admin/admin-ajax.php',
+    wp_uri : 'http://riplive.local/api',
+    admin_uri : 'http://riplive.local/wp-admin/admin-ajax.php',
     memcache_uri : '127.0.0.1:11211',
     static_folder: 'app'
 };
