fd7809b01341d96fe7ae6269cd9f20ed428aa0ee
gabrieledarrigo <darrigo.g@gmail.com>
2015-05-16 11:26:00 +0200

    Removed deprecated test

--- test/spec/server/services/memcached-broker.js
+++ test/spec/server/services/memcached-broker.js
@@ -1,58 +1,76 @@
-var expect          = require('expect.js');
-var memcachedBroker = require(process.cwd() + '/server/services/memcached-broker.js');
-var fakeKey         = 'key';
-var fakeValue       = 'this.is.a.value';
+var expect      = require('expect.js');
+var sinon       = require('sinon');
+var memcached   = require(process.cwd() + '/server/services/memcached-client.js');
+var HttpService = require(process.cwd() + '/server/services/http-service.js');
+var hasher      = require(process.cwd() + '/server/services/hasher.js');
+var Broker      = require(process.cwd() + '/server/services/memcached-broker.js');
+var broker      = new Broker(memcached, HttpService);
+var fakeKey     = 'key';
+var fakeValue   = 'this.is.a.value';
 
 describe('MemcachedBroker', function() {
-    it('is an object used to persists and cache data into a memcached server instance' +
-    	'or to retrieve data from a remote endpoint ', function() {
-
-        expect(memcachedBroker).to.be.an('object');
-        expect(memcachedBroker).to.have.property('setTime');
-        expect(memcachedBroker).to.have.property('getTime');
-        expect(memcachedBroker).to.have.property('set');
-        expect(memcachedBroker).to.have.property('get');
-
-        describe('#setTime', function() {
-            it('should set the default caching lifetime of the broker', function() {
-                var time = 60;
-
-                memcachedBroker.setTime(60);
-                var settedTime = memcachedBroker.getTime();
-
-                expect(settedTime).to.be.equal(time);
-            });
+    beforeEach(function() {
+        sinon.stub(memcached, 'get', function(key, cb) {
+            return cb(null, false);
         });
 
-        describe('#getTime', function() {
-            it('should return the default caching time of the broker', function() {
-                var defaultTime = memcachedBroker.getTime();
-                expect(defaultTime).to.be.equal(60);
-            });
-        });
-
-        describe('#set', function() {
-            it('should set a value into memcached', function(done) {
-
-                memcachedBroker.set(fakeKey, fakeValue, function(err, res) {
-                    if (err) throw err;
-
-                    expect(res).to.be.equal(fakeValue);
-                    done();
-                });
-            });
-        });
-
-        describe('#get', function() {
-            it('should return data from memcached or from a remote endpoint (if passed as arguments)', 
-            	function(done) {
-                memcachedBroker.get(fakeKey, null, function(err, res) {
-                	if (err) throw err;
-
-                	expect(res).to.be.equal(fakeValue);
-                	done();
-                });
-            });
+        sinon.stub(memcached, 'set', function(key, value, lifetime, cb) {
+            return cb(null, value);
         });
     });
+
+    afterEach(function() {
+        memcached.get.restore();
+        memcached.set.restore();
+    });
+
+    it('is an object used to persists and cache data into a memcached server instance' +
+        'or to retrieve data from a remote endpoint ',
+        function() {
+            expect(broker).to.be.an('object');
+            expect(broker).to.have.property('setTime');
+            expect(broker).to.have.property('getTime');
+            expect(broker).to.have.property('set');
+            expect(broker).to.have.property('get');
+
+            describe('#setTime', function() {
+                it('should set the default caching lifetime of the broker', function() {
+                    broker.setTime(60);
+                    var time = broker.getTime();
+
+                    expect(time).to.be.equal(60);
+                });
+            });
+
+            describe('#getTime', function() {
+                it('should return the default caching time of the broker', function() {
+                    var defaultTime = broker.getTime();
+                    expect(defaultTime).to.be.equal(60);
+                });
+            });
+
+            describe('#set', function() {
+                it('should set a value into memcached', function(done) {
+
+                    broker.set(fakeKey, fakeValue, function(err, res) {
+                        if (err) throw err;
+
+                        expect(res).to.be.equal(fakeValue);
+                        done();
+                    });
+                });
+            });
+
+            describe('#get', function() {
+                it('should return data from memcached or from a remote endpoint (if passed as arguments)',
+                    function(done) {
+                        broker.get(fakeKey, null, function(err, res) {
+                            if (err) throw err;
+
+                            expect(res).to.be.equal(fakeValue);
+                            done();
+                        });
+                    });
+            });
+        });
 });
