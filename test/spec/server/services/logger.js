db7a2a6af35b8497d4c8a567ba1c397d0f7f91f8
gabrieledarrigo <darrigo.g@gmail.com>
2015-09-19 13:36:51 +0200

    Update all packages. Travis build

--- test/spec/server/services/logger.js
+++ test/spec/server/services/logger.js
@@ -4,10 +4,6 @@ var logger = require(process.cwd() + '/server/services/logger.js');
 describe('Logger', function() {
     it('is an object used to create log file and stream', function() {
         expect(logger).to.be.an('object');
-        expect(logger).to.have.property('streams');
-        expect(logger).to.have.property('fields');
-        expect(logger.fields).to.be.an('object');
-        expect(logger.fields).to.have.property('name');
-        expect(logger.fields.name).to.be.equal('riplive');
+        expect(logger).to.have.property('transports');
     });
 });
